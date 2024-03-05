a2655e7282398e81a0875d42e80a834726514a28
Nicolas Waitzel <koolbuyers@gmail.com>
2024-03-05 16:47:38 -0300

    wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'articles/new', to: 'articles#new', as: :new_article
+  post 'articles', to: 'articles#create'
+  get 'articles', to: 'articles#index'
+  get 'articles/:id', to: 'articles#show', as: :article
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+# TODO: Write a seed to insert 100 articles in the database
+10.times do
+  article = Article.create(title: Faker::Music.band, content: Faker::Quote.famous_last_words)
+  article.save!
+end
